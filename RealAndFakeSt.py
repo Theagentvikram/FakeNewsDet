8b0cbe94b741ec9a3c2b3924676f52b5d640240d
The Agent <111900847+Theagentvikram@users.noreply.github.com>
2023-07-09 17:22:47 +0530

    Update RealAndFakeSt.py

--- RealAndFakeSt.py
+++ RealAndFakeSt.py
@@ -5,19 +5,20 @@ import re
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.metrics import accuracy_score
 import nltk
 
 nltk.download('stopwords')
 stop_words = stopwords.words('english')
+port_stem = PorterStemmer()
 
 def preprocess_text(content):
-    ps = PorterStemmer()
     stemmed_content = re.sub('[^a-zA-Z]', ' ', content)
     stemmed_content = stemmed_content.lower()
     stemmed_content = stemmed_content.split()
-    stemmed_content = [ps.stem(word) for word in stemmed_content if word not in stop_words]
+    stemmed_content = [port_stem.stem(word) for word in stemmed_content if word not in stop_words]
     stemmed_content = ' '.join(stemmed_content)
     return stemmed_content
 
@@ -32,33 +33,53 @@ st.title("Fake News Detection")
 
 uploaded_true = st.file_uploader("Upload True.csv", type="csv")
 uploaded_fake = st.file_uploader("Upload Fake.csv", type="csv")
-st.write("Wait for the Processing...")
+
 if uploaded_true is not None and uploaded_fake is not None:
     df_true = pd.read_csv(uploaded_true)
     df_fake = pd.read_csv(uploaded_fake)
 
     # Preprocess the data
-    df_true['content'] = df_true['title'] + ' ' + df_true['text'] + ' ' + df_true['date']
+    df_true['content'] = df_true['title'] + ' ' + df_true['text'] + ' ' + df_true['subject'] + ' ' + df_true['date']
     df_true['content'] = df_true['content'].apply(preprocess_text)
 
-    df_fake['content'] = df_fake['title'] + ' ' + df_fake['text'] + ' ' + df_fake['date']
+    df_fake['content'] = df_fake['title'] + ' ' + df_fake['text'] + ' ' + df_fake['subject'] + ' ' + df_fake['date']
     df_fake['content'] = df_fake['content'].apply(preprocess_text)
 
     # Combine the datasets
     df = pd.concat([df_true, df_fake], ignore_index=True)
 
-    # Train the model
-    vectorizer = TfidfVectorizer()
-    x = vectorizer.fit_transform(df['content'])
+    # Separate the data and label
+    X = df['content'].values
     y = np.concatenate([np.zeros(len(df_true)), np.ones(len(df_fake))])
+
+    # Split the dataset into training and testing subsets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Vectorize the text data
+    vectorizer = TfidfVectorizer()
+    X_train_vectorized = vectorizer.fit_transform(X_train)
+    X_test_vectorized = vectorizer.transform(X_test)
+
+    # Train the model
     model = LogisticRegression()
-    model.fit(x, y)
+    model.fit(X_train_vectorized, y_train)
 
+    # Evaluate the model
+    y_train_pred = model.predict(X_train_vectorized)
+    train_accuracy = accuracy_score(y_train, y_train_pred)
+
+    y_test_pred = model.predict(X_test_vectorized)
+    test_accuracy = accuracy_score(y_test, y_test_pred)
+
+    # User input
     text = st.text_area("Enter the news text:")
-    prediction = predict_fake_news(text)
+    if text:
+        prediction = predict_fake_news(text)
+        if prediction == 0:
+            st.write("The news is Real")
+        else:
+            st.write("The news is Fake")
 
-    if prediction == 0:
-        st.write("The news is Real")
-    else:
-        st.write("The news is Fake")
-#sree
+    # Display the accuracies
+    st.write("Training Accuracy:", train_accuracy)
+    st.write("Testing Accuracy:", test_accuracy)
