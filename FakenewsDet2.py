4cc34520865736d3fc4c1ae5be91c0878a98e097
The Agent <111900847+Theagentvikram@users.noreply.github.com>
2023-07-12 22:50:28 +0530

    Update FakenewsDet2.py

--- FakenewsDet2.py
+++ FakenewsDet2.py
@@ -1,52 +1,87 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
+import nltk
 
-# Read the Fake news CSV file
+nltk.download('stopwords')
+stop_words = stopwords.words('english')
 
-fake_news_df = pd.read_csv(r"C:\Users\abhic\Downloads\Fake.csv")
+def preprocess_text(content):
+    ps = PorterStemmer()
+    stemmed_content = re.sub('[^a-zA-Z]', ' ', content)
+    stemmed_content = stemmed_content.lower()
+    stemmed_content = stemmed_content.split()
+    stemmed_content = [ps.stem(word) for word in stemmed_content if word not in stop_words]
+    stemmed_content = ' '.join(stemmed_content)
+    return stemmed_content
 
-# Read the real news CSV file
-real_news_df = pd.read_csv(r"C:\Users\abhic\Downloads\True.csv")
+def predict_fake_news(text):
+    preprocessed_text = preprocess_text(text)
+    vectorized_text = vectorizer.transform([preprocessed_text])
+    prediction = model.predict(vectorized_text)[0]
+    return prediction
 
-# Assign labels: 0 for fake news, 1 for real news
-fake_news_df['label'] = 0
-real_news_df['label'] = 1
+# Streamlit App
+st.title("Fake News Detection")
 
-# Combine the dataframes into a single dataframe
-news_df = pd.concat([fake_news_df, real_news_df], ignore_index=True)
+uploaded_true = st.file_uploader("Upload True.csv", type="csv")
+uploaded_fake = st.file_uploader("Upload Fake.csv", type="csv")
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(news_df['text'], news_df['label'], test_size=0.2, random_state=42)
+if uploaded_true is not None and uploaded_fake is not None:
+    df_true = pd.read_csv(uploaded_true)
+    df_fake = pd.read_csv(uploaded_fake)
 
-# Create a TF-IDF vectorizer
-vectorizer = TfidfVectorizer(stop_words='english')
+    # Looking and replacing null data
+    df_true = df_true.fillna('')
+    df_fake = df_fake.fillna('')
 
-# Fit and transform the training data
-X_train_tfidf = vectorizer.fit_transform(X_train).toarray()
+    # Merging author name and news title for easier use
+    df_true['content'] = df_true['title'] + ' ' + df_true['text'] + ' ' + df_true['date']
+    df_fake['content'] = df_fake['title'] + ' ' + df_fake['text'] + ' ' + df_fake['date']
 
-# Transform the testing data
-X_test_tfidf = vectorizer.transform(X_test).toarray()
+    # Preprocess the data
+    df_true['content'] = df_true['content'].apply(preprocess_text)
+    df_fake['content'] = df_fake['content'].apply(preprocess_text)
 
-# Convert labels to NumPy arrays
-y_train = np.array(y_train)
-y_test = np.array(y_test)
+    # Combine the datasets
+    df = pd.concat([df_true, df_fake], ignore_index=True)
 
-# Train a logistic regression model
-logreg = LogisticRegression()
-logreg.fit(X_train_tfidf, y_train)
+    x = df['content'].values
+    y = df['label'].values
 
-# Predict the labels for the testing data
-y_pred = logreg.predict(X_test_tfidf)
+    # Converting the textual data to numerical data for easier use
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit(x)
+    x = vectorizer.transform(x)
 
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-confusion_mat = confusion_matrix(y_test, y_pred)
-classification_rep = classification_report(y_test, y_pred)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y, random_state=2)
+    model = LogisticRegression()
+    model.fit(x_train, y_train)
 
-print('Accuracy:', accuracy)
-print('Confusion Matrix:\n', confusion_mat)
-print('Classification Report:\n', classification_rep)
+    x_train_pred = model.predict(x_train)
+    training_data_acc = accuracy_score(x_train_pred, y_train)
+
+    x_test_pred = model.predict(x_test)
+    test_data_acc = accuracy_score(x_test_pred, y_test)
+
+    st.write(f"Training Accuracy: {training_data_acc}")
+    st.write(f"Test Accuracy: {test_data_acc}")
+
+    text = st.text_area("Enter the news text:")
+    
+    if st.button("Predict"):
+        if text:
+            with st.spinner("Predicting..."):
+                prediction = predict_fake_news(text)
+
+            if prediction == 0:
+                st.write("The news is Real")
+            else:
+                st.write("The news is Fake")
