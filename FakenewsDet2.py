2aaf5aa6727dd4f016007d2cceebcd27daba1d3d
The Agent <111900847+Theagentvikram@users.noreply.github.com>
2023-07-14 22:24:08 +0530

    Update FakenewsDet2.py

--- FakenewsDet2.py
+++ FakenewsDet2.py
@@ -12,7 +12,7 @@ import nltk
 
 nltk.download('stopwords')
 stop_words = stopwords.words('english')
-
+st.set_page_config(page_title="Fake News Detection", page_icon="IMG.png")
 def preprocess_text(content):
     ps = PorterStemmer()
     stemmed_content = re.sub('[^a-zA-Z]', ' ', content)
